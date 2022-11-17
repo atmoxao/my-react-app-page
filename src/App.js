97ce3c874065243587d340b2b1f06cf20364aa10
彭畅 <changpeng94@gmai.com>
2022-11-17 19:08:55 +0800

    strtoupper app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EDIT <CODE>SRC/APP.JS</CODE> AND SAVE TO RELOAD.
         </p>
         <a
           className="App-link"

a8c12f356e43016e6a32d5cbf0020837682d5a95
moxao <32735848+atmoxao@users.noreply.github.com>
2022-12-07 09:51:51 +0000

    fix: codeSpaces test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           edit <code>src/app.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi,Moxao~
-        </a>
       </header>
     </div>
   );
